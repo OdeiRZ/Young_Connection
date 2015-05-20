541cde183bbe318cb3a2ff9ae6d287bbbcf4fe93
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2015-05-20 17:12:23 +0200

    Agregado fixtures para crear usuario admin

---
+++ src/AppBundle/DataFixtures/ORM/Usuarios.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\Usuario;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class Usuarios extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function getOrder()
+    {
+        return 100;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $encoder = $this->container->get('security.password_encoder');
+        $usuario = new Usuario();
+        $usuario->setCorreoElectronico('admin@admin.com')
+            ->setPassword($encoder->encodePassword($usuario, 'admin@admin.com'))
+            ->setNombre('Admin')
+            ->setApellidos('1')
+            ->setFechaNacimiento(new \DateTime('1980-05-01'))
+            ->setSexo('M')
+            ->setTelefono('666666666')
+            ->setEsActivo(true)
+            ->setEsAdministrador(true)
+            ->setEsCoordinador(false);
+        $manager->persist($usuario);
+        $manager->flush();
+    }
+}
