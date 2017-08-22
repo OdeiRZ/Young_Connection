3e41a7223ce945ce03047827a9d510a1e5f29831
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2017-08-22 22:06:20 +0200

    Actualizadas referencias de instalación

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -36,4 +36,10 @@ class AppKernel extends Kernel
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
+	
+	public function init()
+	{
+		date_default_timezone_set('Europe/Madrid');
+		parent::init();
+	}
 }
